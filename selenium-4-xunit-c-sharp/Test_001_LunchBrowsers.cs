4cc5fc35366f20fcf0f62bf6c732d295469b3d23
bija510 <error.zxz@gmail.com>
2025-03-07 22:47:54 -0500

    Added 1st Script Lunch Browers

---
+++ selenium-4-xunit-c-sharp/Test_001_LunchBrowsers.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+
+namespace selenium_4_xunit_c_sharp
+{
+    public class Test_001_LunchBrowsers
+    {
+        protected IWebDriver driver;
+        [Fact]
+        public void test_lunch_chrome()
+        {    
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.facebook.com");
+            Thread.Sleep(4000);
+            driver.Quit();
+        }
+        [Fact]
+        public void test_lunch_edge()
+        {
+            driver = new EdgeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.amazon.com");
+            Thread.Sleep(4000);
+            driver.Quit();
+
+        }
+        [Fact]
+        public void test_lunch_firefox()
+        {
+            driver = new FirefoxDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.walmart.com");
+            Thread.Sleep(4000);
+            driver.Quit();
+
+        }
+    }
+}
